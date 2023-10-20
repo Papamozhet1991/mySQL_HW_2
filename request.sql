ecf9d8741d8abf7b22dfb6990393a78b08215f40
Artyom Skovorodko <papamozhet1991@yandex.ru>
2023-10-20 12:11:41 +0500

    Add changes

--- request.sql
+++ request.sql
@@ -1,3 +1,5 @@
-select ORDERS.product_name, CUSTOMERS.name 
-from ORDERS, CUSTOMERS
-where ORDERS.customer_id  = CUSTOMERS.id  and CUSTOMERS.name = 'алексей';
+select ORDERS.product_name, CUSTOMERS.name
+from CUSTOMERS
+inner join  ORDERS  
+on ORDERS.customer_id  = CUSTOMERS.id 
+and CUSTOMERS.name = 'алексей';
